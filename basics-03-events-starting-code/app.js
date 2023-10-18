86ad8fc23c75a97f11bf7a66e787dc047d064245
ahmetyasir99 <ahmetyasir.cilvez@agu.edu.tr>
2023-10-19 00:22:48 +0300

    section 2 class 3

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,28 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: ''
     };
   },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce() {
+      this.counter--;
+    },
+    setName(event, par2) { //event default bir parametre oldugu için htmlde vermemize gerek yok, verince
+      //çalışmıyor zaten.
+      this.name = event.target.value + par2;
+    },
+    submitForm() {
+      alert('submitted');
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    }
+  }
 });
 
 app.mount('#events');
