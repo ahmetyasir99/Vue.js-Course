d4d8d322698b81d9201cf381a3562dbe93e0192c
ahmetyasir99 <ahmetyasir.cilvez@agu.edu.tr>
2024-01-13 20:46:51 +0300

    news and project

---
+++ projeler/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,87 @@
+<template>
+  <base-card>
+    <base-button
+      :mode="storedResButtonMode"
+      @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button
+      :mode="addResButtonMode"
+      @click="setSelectedTab('add-resource')"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <KeepAlive>
+    <component :is="selectedTab"></component>
+  </KeepAlive>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn how to google',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addResource,
+      removeResource: this.removeResource
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    addResource(title, description, url) {
+        const newResource = {
+            id: new Date().toISOString,
+            title,
+            description,
+            link: url
+        };
+
+        this.storedResources.unshift(newResource);
+        this.selectedTab = 'stored-resources'
+    },
+    removeResource(resId) {
+        const resIndex = this.storedResources.findIndex((res) => res.id === resId);
+
+        this.storedResources.splice(resIndex, 1)
+        //this.storedResources = this.storedResources.filter(res => res.id!== resId);
+        //aynı arrayı manüpliye edip yeniden setlemek child elementlerede çalışmaz,
+        //bu yüzden orijinal arryı manüplie etmeliyiz.
+        //yeniden setleme yapma orijinali değiştir
+    }
+  },
+};
+</script>
